025b1486c450da17d2174fd526ab4e7b20ff9821
Matthew Wong <pillows@users.noreply.github.com>
2019-04-11 18:57:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/matt/cloudplay/PlayerActivity.java
@@ -0,0 +1,109 @@
+package com.example.matt.cloudplay;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.example.rithuusharrma.tunes.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.jar.Manifest;
+
+public class PlayerActivity extends AppCompatActivity{
+    static MediaPlayer mp;//assigning memory loc once or else multiple songs will play at once
+    int position;
+    SeekBar sb;
+    ArrayList<File> mySongs;
+    Thread updateSeekBar;
+    Button pause,forward,reverse,next,previous;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        pause = (Button)findViewById(R.id.pause);
+        forward = (Button)findViewById(R.id.forward);
+        previous = (Button)findViewById(R.id.previous);
+        next = (Button)findViewById(R.id.next);
+        reverse = (Button)findViewById(R.id.reverse);
+        sb=(SeekBar)findViewById(R.id.seekBar);
+
+        updateSeekBar=new Thread(){
+            @Override
+            public void run(){
+                int totalDuration = mp.getDuration();
+                int currentPosition = 0;
+                //sb.setMax(totalDuration);
+                while(currentPosition < totalDuration){
+                    try{
+                        sleep(500);
+                        currentPosition=mp.getCurrentPosition();
+                        sb.setProgress(currentPosition);
+                    }
+                    catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        };
+        if(mp != null){
+            mp.stop();
+            mp.release();
+        }
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        mySongs = (ArrayList) b.getParcelableArrayList("songs");
+        position = b.getInt("pos",0);
+        Uri u = Uri.parse(mySongs.get(position).toString());
+        mp = MediaPlayer.create(getApplicationContext(),u);
+
+        mp.start();
+        sb.setMax(mp.getDuration());
+        updateSeekBar.start();
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mp.seekTo(seekBar.getProgress());
+            }
+
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sb.setMax(mp.getDuration());
+
+                if(mp.isPlaying()){
+                    pause.setText(">");
+                    mp.pause();
+                }
+                else {
+                    pause.setText("||");
+                    mp.start();
+                }
+
+            }
+        });
+
+    }}
